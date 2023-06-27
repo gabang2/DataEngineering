31dacbd70475071d989eefada4adfa311e7c807e
gabang2 <offbeat1020@naver.com>
2023-06-27 12:02:44 +0900

    변수와 상수

---
+++ java_utility_gradle/src/main/java/com/fastcampus/de/clip6/Constant.java
@@ -0,0 +1,11 @@
+package com.fastcampus.de.clip6;
+
+public class Constant {
+    public static void main(String[] args) {
+        final int number = 1;
+        System.out.println(number);
+        // 컴파일 에러✨
+        // number = 3;
+        System.out.println(number);
+    }
+}
---
+++ java_utility_gradle/src/main/java/com/fastcampus/de/clip6/Variable.java
@@ -0,0 +1,20 @@
+package com.fastcampus.de.clip6;
+
+public class Variable {
+    public static void main(String[] args) {
+
+        // 변수와 상수 선언하는 방법
+        int number = 1;
+        System.out.println(number);
+
+        String str = "hello world";
+        System.out.println(str);
+
+        str = "good by world";
+        System.out.println(str);
+
+        // camel case 이해하기
+        int myVariable = 2;
+        int myNumberVariable = 3;
+    }
+}
