5ad745e79f0bc1ebae382a00ebe8bb81713fb854
gabang2 <offbeat1020@naver.com>
2023-06-30 22:02:06 +0900

    Java : Generics 이해하기 - Collection클래스를 통해!

---
+++ java_utility_gradle/src/main/java/com/fastcampus/de/clip15/Main.java
@@ -0,0 +1,13 @@
+package com.fastcampus.de.clip15;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList();
+        Collection<String> collection = list;
+    }
+}
